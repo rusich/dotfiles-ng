2a6e35fc1fa3762ed4c13ba735d69f4a51790fd2
Ruslan Sergin <ruslan.sergin@gmail.com>
2026-01-05 01:55:11 +0900

    move to single-file overlay.nix

--- overlays.nix
+++ overlays.nix
@@ -1,7 +1,14 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
-  # When applied, the stable unstable nixpkgs sety (declared in the flake inputs)
-  # will be accessible through 'pkgs.unstable'
+
+  tonelib-gfx = pkgs.tonelib-gfx.overrideAttrs (old: {
+    version = "4.9.0"; # New version
+    src = pkgs.fetchurl {
+      url = "https://tonelib.vip/download/25-11-10/ToneLib-GFX-amd64.deb"; # Source URL
+      sha256 = "sha256-<new-hash>"; # Replace <new-hash> with the correct SHA256 hash
+    };
+  });
+
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
       # system = final.system;

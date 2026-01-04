ef37f5623cdc4197c6e6eefa8d6a8822054c6cf6
Ruslan Sergin <ruslan.sergin@gmail.com>
2026-01-05 02:57:29 +0900

    single overlays.nix works!

--- overlays.nix
+++ overlays.nix
@@ -1,13 +1,13 @@
-{ inputs, pkgs, ... }:
+{ inputs, ... }:
 {
 
-  tonelib-gfx = pkgs.tonelib-gfx.overrideAttrs (old: {
-    version = "4.9.0"; # New version
-    src = pkgs.fetchurl {
-      url = "https://tonelib.vip/download/25-11-10/ToneLib-GFX-amd64.deb"; # Source URL
-      sha256 = "sha256-<new-hash>"; # Replace <new-hash> with the correct SHA256 hash
-    };
-  });
+  # tonelib-gfx = pkgs.tonelib-gfx.overrideAttrs (old: {
+  #   version = "4.9.0"; # New version
+  #   src = pkgs.fetchurl {
+  #     url = "https://tonelib.vip/download/25-11-10/ToneLib-GFX-amd64.deb"; # Source URL
+  #     sha256 = "sha256-<new-hash>"; # Replace <new-hash> with the correct SHA256 hash
+  #   };
+  # });
 
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {

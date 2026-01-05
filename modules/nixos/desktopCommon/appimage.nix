94a98d496a7d63cd30e7f43a28962c1de0ad0dcc
Ruslan Sergin <ruslan.sergin@gmail.com>
2026-01-05 20:18:32 +0900

    appimage support

--- modules/nixos/desktopCommon/appimage.nix
+++ modules/nixos/desktopCommon/appimage.nix
@@ -1,13 +1,25 @@
 { pkgs, ... }:
 {
 
-  # Exec AppImage files directly
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    recognitionType = "magic";
-    offset = 0;
-    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-    magicOrExtension = ''\x7fELF....AI\x02'';
+  # environment.systemPackages = with pkgs; [
+  #   appimage-run
+  # ];
+  #
+  # # Exec AppImage files directly
+  # boot.binfmt.registrations.appimage = {
+  #   wrapInterpreterInShell = false;
+  #   interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+  #   recognitionType = "magic";
+  #   offset = 0;
+  #   mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+  #   magicOrExtension = ''\x7fELF....AI\x02'';
+  # };
+
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
+  programs.appimage.package = pkgs.appimage-run.override {
+    extraPkgs = pkgs: [
+      pkgs.python312
+    ];
   };
 }

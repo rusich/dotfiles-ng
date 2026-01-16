bdf7a12598cf49453a73686a2855c29a3e99bf3a
Ruslan Sergin <ruslan.sergin@gmail.com>
2026-01-16 20:33:29 +0900

    nvim: obsolete obsvim configuration deleted

--- legacy-configs/obsvim/init.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
-
-require 'config.lazy-bootstrap'
-require 'config.options'
-require 'config.lsp'
-require 'config.keymaps'
-require 'config.autocommands'
-require 'config.highlights'

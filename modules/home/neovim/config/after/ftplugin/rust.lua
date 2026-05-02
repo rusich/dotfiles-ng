3a0cd27d207b7ee034d276b1049a55b6f989b3d2
Ruslan Sergin <ruslan.sergin@gmail.com>
2026-05-02 20:12:19 +0900

    feat(neovim): add 'K' keymap override for rustaceanvim hover actions
    Override Neovim's built-in hover keymap in Rust ftplugin to use
    rustaceanvim's hover actions via 'K', providing rich hover
    functionality with action menus.

--- modules/home/neovim/config/after/ftplugin/rust.lua
+++ modules/home/neovim/config/after/ftplugin/rust.lua
@@ -1,3 +1,12 @@
 print 'hello from rust ftplugin'
 vim.keymap.set('n', 'J', '<cmd>RustLsp joinLines<CR>')
 vim.keymap.set('n', '<leader>ce', '<cmd>RustLsp explainError<CR>', { desc = 'Rust: Explain error' })
+
+vim.keymap.set(
+  'n',
+  'K', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+  function()
+    vim.cmd.RustLsp { 'hover', 'actions' }
+  end,
+  { silent = true, buffer = bufnr }
+)

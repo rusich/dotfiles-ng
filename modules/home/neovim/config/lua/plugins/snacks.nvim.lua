0bb4688b3b0b47ce26486a33d44dcc3ab1f914ff
Ruslan Sergin <ruslan.sergin@gmail.com>
2026-09-01 16:18:20 +0900

    feat(nvim): add GitHub issues and PR pickers to snacks
    Adds gh sources to the snacks picker and enables the gh module so
    issues and pull requests can be browsed without leaving Neovim.
    
    - <leader>gi / <leader>gI for open/all GitHub issues
    - <leader>gp / <leader>gP for open/all GitHub pull requests

--- modules/home/neovim/config/lua/plugins/snacks.nvim.lua
+++ modules/home/neovim/config/lua/plugins/snacks.nvim.lua
@@ -30,6 +30,10 @@ local spec = {
           require('opencode').prompt(table.concat(items, ', ') .. ' ')
         end,
       },
+      sources = {
+        gh_issue = {},
+        gh_pr = {},
+      },
     },
     image = {
       debug = {
@@ -93,6 +97,9 @@ local spec = {
       },
     },
     debug = { enabled = true },
+    gh = {
+      enabled = true,
+    },
     git = { enabled = true },
     input = {
       enabled = true,
@@ -154,6 +161,34 @@ local spec = {
         Snacks.picker.git_log_line()
       end,
       desc = 'Git Blame Line',
+    },
+    {
+      '<leader>gi',
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = 'GitHub Issues (open)',
+    },
+    {
+      '<leader>gI',
+      function()
+        Snacks.picker.gh_issue({ state = 'all' })
+      end,
+      desc = 'GitHub Issues (all)',
+    },
+    {
+      '<leader>gp',
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = 'GitHub Pull Requests (open)',
+    },
+    {
+      '<leader>gP',
+      function()
+        Snacks.picker.gh_pr({ state = 'all' })
+      end,
+      desc = 'GitHub Pull Requests (all)',
     },
     {
       '<leader>gf',

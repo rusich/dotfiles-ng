7582825e2daf8b21a03b3bce228409bb726cefcd
Ruslan Sergin <ruslan.sergin@gmail.com>
2026-09-09 12:53:02 +0900

    feat(nix): use unstable.steam in gaming module
    Switch the Steam package to the unstable channel for a newer client.

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -33,6 +33,7 @@ in
     programs.steam = {
       enable = true;
       # package = pkgs.millennium-steam;
+      package = pkgs.unstable.steam;
       gamescopeSession.enable = true;
       protontricks.enable = true;
       remotePlay.openFirewall = true;

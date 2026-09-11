214745913f85c780d1a208912ab931828b69fede
Ruslan Sergin <ruslan.sergin@gmail.com>
2026-09-11 16:52:27 +0900

    fix(nix): update broadcom-sta insecure package to kernel 6.18.49
    Permitted insecure package version bumped to match the 6.18.49
    kernel in the wireless driver package hash.

--- hosts/nixos/MacBook-Air/configuration.nix
+++ hosts/nixos/MacBook-Air/configuration.nix
@@ -139,7 +139,7 @@
   ];
 
   nixpkgs.config.permittedInsecurePackages = [
-    "broadcom-sta-6.30.223.271-59-6.18.37"
+    "broadcom-sta-6.30.223.271-59-6.18.49"
   ];
 
   boot.blacklistedKernelModules = [ "b43" ];

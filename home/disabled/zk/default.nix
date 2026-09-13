ceaf3efa0d21d32143c6c4426af94776c74b70dd
Ruslan Sergin <ruslan.sergin@gmail.com>
2026-09-14 06:52:45 +0900

    chore(home): drop unused disabled configs
    Remove home/disabled/** (fzf, stylix, zk). These files are not imported by
    any module and are no longer used.

--- home/disabled/zk/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }: {
-  programs.zk.enable = true;
-
-  # backup nvim config
-  home.file = {
-    ".config/zk/config.toml".source =
-      config.lib.file.mkOutOfStoreSymlink config.homeModulesPath + "/zk/config.toml";
-  };
-}
